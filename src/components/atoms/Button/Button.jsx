44f538877e29c15fdb7a87da4d8595b3139ddbe5
FranzPalgo <soporte.ti.palgo@gmail.com>
2023-03-28 13:09:21 -0500

    update

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './stylesButton.css'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
 
-export const Button =() => (
-    <button className="primary">
-        Aceptar
-    </button>
-);
+// export const Button = ({type, isDisabled}) => {
+//     let classType = ""
+//     let isdisabled = ""
+//     isdisabled = (isDisabled) ? "is-disabled" : ""
+//     classType = (type) ? `type-${type} ` : ""
+//     return (
+//     <button className={classType + "button" + " " + isdisabled}>
+//         Aceptar
+//     </button>)
+// }
+
+export const Button = ({type, isDisabled, children}) => {
+    return (
+    <button 
+        className={classNames('button', 
+        {[`type-${type}`] : type}, 
+        {'is-disabled' : isDisabled})}
+    >
+        {children}
+    </button>)
+}
+
+Button.propTypes = {
+    children: PropTypes.string,
+    isDisabled: PropTypes.bool,
+    type: PropTypes.oneOf(["primary", "secondary"])
+}
+
+Button.defaultProps = {
+    children: "Boton",
+    isDisabled: false,
+    type: "primary"
+}
--- src/components/atoms/Button/Button.stories.jsx
+++ src/components/atoms/Button/Button.stories.jsx
@@ -4,5 +4,16 @@ import {Button} from './Button'
 export default{
     title: 'atomo/Boton',
     component: Button,
+    args: {
+        children: 'boton',
+        isDisabled: false,
+    },
+    argTypes: {
+        children: { control: 'text' },
+        type: { control: 'text' },
+    }
 }
-export const Default = () => <Button/> 
+export const Default = () => <Button/>
+export const Primary = () => <Button type="primary">Primary</Button>
+export const Secondary = () => <Button type="secondary">Secondary</Button>
+export const IsDisabled = () => <Button type="primary" isDisabled = {true}>Disabled</Button>
